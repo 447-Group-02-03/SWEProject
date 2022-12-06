8d051c9929a30432e8c5a095410a4fa2b3a6723e
Dagem Sinke <dagemsinke@Dagems-MacBook-Pro.local>
2022-12-05 21:32:01 -0500

    made css changes

--- swe-project-client/src/pages/Home.js
+++ swe-project-client/src/pages/Home.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <h2>Home</h2>
-          <Link to="/blackjack">
-            <button>Blackjack</button>
-          </Link>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <Card style={{ color: "#000", marginBottom: 15 }}>
+            <Card.Body>
+              <Card.Title> Welcome to Card Stop!</Card.Title>
+              <Card.Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjJSOJive1GZiEC4jiMUMbrDUhsqKlh-6bdw&usqp=CAU" />
+              <Card.Text>Select Game</Card.Text>
+              <Link to="/blackjack">
+                <Button type="button" class="btn btn-warning">
+                  Blackjack
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </header>
+      </div>
     );
   }
 }
